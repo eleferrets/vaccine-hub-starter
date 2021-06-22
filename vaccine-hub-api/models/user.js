fc595e5af43dc77ce02489fa82142e36e91ccc29
elefe <eleferrets@gmail.com>
2021-06-22 13:38:24 -0400

    Left in notes for implementation and setup base

---
+++ vaccine-hub-api/models/user.js
@@ -0,0 +1,29 @@
+const {UnauthorizedError} = require("../utils/errors")
+
+class User {
+    static async login(credentials) {
+        // user should submit email and password
+        // if any fields are missing, throw an error
+
+        // lookup the user in the db by email
+        // if a user is found, compare the submitted password
+        // with the password in the db
+        // if is a match, return user
+
+        // if anything else is wrong, throw an error
+        throw new UnauthorizedError("Invalid email/password combo")
+    }
+    static async register(credentials) {
+// user should submit email, pw, rsvp status, and # of guests
+// if any of fields are missing, throw error
+// make sure no user already exists in the system with that email
+// if one does, throw and error
+
+// take the users password, and hash it
+// take the users email and lowercase it
+
+// create a new user in the db with all info
+// return the user
+    }
+}
+module.exports = User
--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
 
+const {BadRequestError, NotFoundError} = require("./utils/errors")
+
 const app = express()
 
 // enable cross-origin resource sharing
@@ -11,6 +13,20 @@ app.use(express.json())
 // log request info
 app.use(morgan("tiny"))
 
+// We didn't reach any endpoint
+app.use((req, res, next) => {
+    return next(new NotFoundError())
+});
+
+// Generic error handler
+app.use((err, req, res, next) => {
+    const status = err.status || 500
+    const message = err.message
+
+    return res.status(status).json({
+        error: {message, status},
+    })
+})
 // Environment variable or the port we are accustomed to
 const PORT = process.env.PORT || 3001
 
